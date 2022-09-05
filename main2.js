5f496894b1184201384d956a02fc4dec181cff36
EC2 Default User <ec2-user@ip-172-31-28-125.us-west-2.compute.internal>
2022-09-05 15:41:45 +0000

    完成分

--- main2.js
+++ main2.js
@@ -1,56 +1,51 @@
-var result = "";
-var is_calc = false;
+let result = document.getElementById("result");
+let data = "";
+let is_calc = false;
 
-window.onload=function(){
-    result = document.getElementById('result');
-};
-
-function c_click(){
-    result.value = "0";
-    is_calc = false;
-}
-
-function num_click(val){
-    if(is_calc)result.value = "0";
-    is_calc = false;
-    
-    if(result.value == "0" && val == "0"){
-        result.value = "0";
-    }else if(result.value == "0" && val == "."){
-        result.value = "0.";
-    }else if(result.value == "0"){
-        result.value = val;
-    }else{
-        result.value += val;
-    }
-}
 
 function ope_click(val){
-    if(is_calc){
-        is_calc = false;
-    }
+    if(is_calc) is_calc = false;
+    
     if(is_ope_last()){
-        result.value = result.value.slice(0, -1) + val;
+        result.value = result.value.slice(0,-1) + val;
     }else{
         result.value += val;
-    }
-}
-
-function equal_click(){
-    if(is_ope_last()){ 
-        result.value = result.value.slice(0, -1);
-    
-        var temp = new Function(result.value.replace("×","*").replace("÷","/"))();
-    }
-    
-    if(temp == Infinity || Number.isNaN(temp)){
-        result.value = "Error";
-    }else{
-        result.value = temp;
-        is_calc = true;
     }
 }
 
 function is_ope_last(){
-    return["+","-","×","÷"].includes(result.value.toString().slice(-1));
+  return ["+","-","*","/"].includes(result.value.toString().slice(-1));
+}
+
+function num_click(value){
+    if(is_calc) result.value = "0";
+    is_calc = false;
+    
+    if(result.value == "0" && value == "0"){
+        result.value = "0";
+    }else if(result.value == "0" && value =="."){
+        result.value = "0.";
+    }else if(result.value == "0"){
+        result.value = value;
+    }else{
+        result.value += value;
+    }
+}
+
+function equal_click(){
+    if(is_ope_last()) result.value.slice(0,-1);
+    result.value = new Function("return " + result.value)();
+    
+    if (ans == Infinity || Number.isNaN(ans)){
+        result.value = "Error";
+    }else{
+        result.value = ans;
+        is_calc = true;
+    }
+}
+
+    
+function c_click(){
+        result.value = "0";
+        is_calc = false;
 }
